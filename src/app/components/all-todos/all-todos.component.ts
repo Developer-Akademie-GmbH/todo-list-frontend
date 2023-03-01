47976ba868276aebed079dafbd307c79ba7e07bb
Junus Ergin <junus91@googlemail.com>
2023-03-01 17:17:32 +0100

    added login form

---
+++ src/app/components/all-todos/all-todos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-todos',
+  templateUrl: './all-todos.component.html',
+  styleUrls: ['./all-todos.component.scss']
+})
+export class AllTodosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  email:string = '';
+  password:string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    // Logik, um mit backend zu kommunizieren
+  }
+
+}
