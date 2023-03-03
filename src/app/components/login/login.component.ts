3b862431151fe371c3b243e1180e092a85fbb0db
Junus Ergin <junus91@googlemail.com>
2023-03-03 11:45:57 +0100

    06 - Login mit Angular App

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,15 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  email:string = '';
-  password:string = '';
+  username: string = '';
+  password: string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  login(){
-    // Logik, um mit backend zu kommunizieren
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    try {
+      let resp = await fetch("http://localhost:8000/login/", requestOptions);
+      let json = await resp.json();
+      localStorage.setItem('token', json.token);
+      // TODO: Redirect
+    } catch(e){
+      // Show error message
+      console.error(e);
+      
+    }
   }
 
 }
